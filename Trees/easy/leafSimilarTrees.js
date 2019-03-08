87212b64ffc6167cb2ca66e686499cd930c70073
Ron Arbel <ronarbel1@gmail.com>
2019-03-07 17:35:43 -0800

    Complete solution

--- Trees/easy/leafSimilarTrees.js
+++ Trees/easy/leafSimilarTrees.js
@@ -1,3 +1,4 @@
+// -------- Initial working solution -------- //
 var leafSimilar = function(root1, root2) {
   const leafValueSequence = (root) => {
       const result = [];
@@ -16,3 +17,20 @@ var leafSimilar = function(root1, root2) {
   }
   return true;
 };
+
+// -------- Refactored solution -------- //
+var leafSimilar = function(root1, root2) {
+  const seq1 = [];
+  const seq2 = [];
+  const dfs = (seq, node) => {
+      if (node.left) dfs(seq, node.left);
+      if (node.right) dfs(seq, node.right);
+      if (!node.left && !node.right) seq.push(node.val);
+  };
+  dfs(seq1, root1);
+  dfs(seq2, root2);
+  for (let i = 0; i < seq1.length; i += 1) {
+      if (seq1[i] !== seq2[i]) return false;
+  }
+  return true;
+}
