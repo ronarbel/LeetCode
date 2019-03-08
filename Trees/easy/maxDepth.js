b7f37b74797606edf1b54e18112b1a2188d672f5
Ron Arbel <ronarbel1@gmail.com>
2019-03-08 14:18:09 -0800

    Refactored solution

--- Trees/easy/maxDepth.js
+++ Trees/easy/maxDepth.js
@@ -3,10 +3,21 @@ var maxDepth = function(root) {
   if (!root) return 0;
   const findDepth = (curDepth, node) => {
       let tempDepth = curDepth;
-      if (node.right) tempDepth = Math.max(findDepth(curDepth + 1, node.right), tempDepth);
-      if (node.left) tempDepth = Math.max(findDepth(curDepth + 1, node.left), tempDepth);
+      if (node.right) {
+        tempDepth = Math.max(findDepth(curDepth + 1, node.right), tempDepth);
+      }
+      if (node.left) {
+        tempDepth = Math.max(findDepth(curDepth + 1, node.left), tempDepth);
+      }
       
       return tempDepth
   }
   return findDepth(1, root);
 };
+
+// -------- refactored solution -------- //
+var maxDepth = function(root) {
+  if (!root) return 0;
+  
+  return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+}
