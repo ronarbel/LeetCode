ece44ebf8fb73dcf010e85d4e3dc68a6517b6cc6
Ron Arbel <ronarbel1@gmail.com>
2019-03-20 18:17:59 -0700

    Complete solution

--- Arrays/easy/findCommonCharacters.js
+++ Arrays/easy/findCommonCharacters.js
@@ -1,31 +1,34 @@
+/* eslint-disable */
 var commonChars = function(A) {
   const words = A.map((word) => {
       const letterMap = {};
       for (let i = 0; i < word.length; i += 1) {
-          if (!letterMap[word[i]]) {
-              letterMap[word[i]] = 1;
+          let letter = word[i];
+          if (!letterMap[letter]) {
+              letterMap[letter] = 1;
           } else {
-              letterMap[word[i]] += 1;
+              letterMap[letter] += 1;
           }
       }
       return letterMap;
   });
   
   let seen = {};
-  for (let i = 0; i < words.length; i += 1) {
+  for (let i = 1; i < words.length; i += 1) {
       const firstWord  = words[0];
+      const currentWord = words[i];
       for (let letter in firstWord) {
-          if (firstWord[letter] && words[i][letter]) {
+          if (firstWord[letter] && currentWord[letter]) {
               if (!seen[letter]) {
                   seen[letter] = {
                       numberOfWordsIn: 1,
-                      charRepeats: Math.min(firstWord[letter], words[i][letter])
+                      minRepeats: Math.min(firstWord[letter], currentWord[letter])
                   }
               } else {
                   seen[letter].numberOfWordsIn += 1;
-                  seen[letter].charRepeats = Math.min(
-                      seen[letter].charRepeats,
-                      words[i][letter]
+                  seen[letter].minRepeats = Math.min(
+                      seen[letter].minRepeats,
+                      currentWord[letter]
                   );
               }
           }
@@ -35,7 +38,7 @@ var commonChars = function(A) {
   const result = []
   for (let letter in seen) {
       if (seen[letter].numberOfWordsIn === A.length) {
-          for (let i = 0; i < seen[letter].charRepeats; i += 1) {
+          for (let i = 0; i < seen[letter].minRepeats; i += 1) {
               result.push(letter);
           }
       }
