9ba05386584d660f8105a72ee3b816d42e6ae940
Ron Arbel <ronarbel1@gmail.com>
2019-03-22 12:56:03 -0700

    Complete solution

--- Arrays/easy/moveZeros.js
+++ Arrays/easy/moveZeros.js
@@ -13,3 +13,24 @@ var moveZeroes = function(nums) {
   }
   return nums;
 };
+
+// suboptimal solution
+var moveZeroes = function(nums) {
+    for (let i = nums.length - 1; i >= 0; i -= 1) {
+        if (nums[i] === 0) nums.push(nums.splice(i, 1));
+    }
+    return nums;
+}
+
+// -------- O(n) time O(1) space -------- //
+var moveZeroes = function(nums) {
+  let idx = 0;
+  for (let i = 0; i < nums.length; i += 1) {
+      if (nums[i] !== 0) {
+          nums[idx] = nums[i];
+          nums[i] = idx === i ? nums[i] : 0;
+          idx += 1;
+      }
+  }
+  return nums;
+}
