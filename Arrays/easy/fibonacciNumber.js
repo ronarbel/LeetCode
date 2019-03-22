a6d8edd5f4a630cf8354c1ddb532efb38eac9ffc
Ron Arbel <ronarbel1@gmail.com>
2019-03-21 18:01:00 -0700

    Complete solution

---
+++ Arrays/easy/fibonacciNumber.js
@@ -0,0 +1,15 @@
+var fib = function(N) {
+  if (N < 1) return 0;
+  if (N === 1 || N == 2) return 1;
+  
+  let prevPrevNum = 1;
+  let prevNum = 1;
+  let current = prevNum + prevPrevNum;
+  while (N > 3) {
+      prevPrevNum = prevNum;
+      prevNum = current;
+      current = prevNum + prevPrevNum
+      N -= 1;
+  }
+  return current
+};
