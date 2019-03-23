2167876a1d522fd3e298139b0fe29eb9cbe953f3
Ron Arbel <ronarbel1@gmail.com>
2019-03-23 14:54:28 -0700

    Complete solution

--- Top100/addTwoNumbers.js
+++ Top100/addTwoNumbers.js
@@ -48,3 +48,34 @@ var addTwoNumbers = function(l1, l2) {
   if (remainder) resTail.next = new ListNode(1);
   return resHead.next
 };
+
+// refactored
+var addTwoNumbers = function(l1, l2) {
+  let cur1 = l1;
+  let cur2 = l2;
+  
+  let resHead = new ListNode('holder');
+  let resTail = resHead
+  let remainder = 0;
+  
+  while (cur1 || cur2) {
+      let sum = remainder;
+      cur1 ? sum += cur1.val : null;
+      cur2 ? sum += cur2.val : null;
+      remainder = 0;
+      
+      if (sum > 9) {
+          sum -= 10;
+          remainder += 1;
+      }
+      
+      resTail.next = new ListNode(sum);
+      resTail = resTail.next;
+      cur1 = cur1 ? cur1.next : null;
+      cur2 = cur2 ? cur2.next : null;
+  }
+  
+  if (remainder) resTail.next = new ListNode(1);
+  
+  return resHead.next
+};
